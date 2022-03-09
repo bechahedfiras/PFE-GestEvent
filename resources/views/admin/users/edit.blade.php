7a1039f3907b8d35425d9ae41ba8695514231771
bechahedfiras <firas_bechahed@hotmail.com>
2022-03-09 13:34:50 +0100

    Dashboard: Edit user

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -11,6 +11,35 @@
                   <form action="{{route('admin.users.update', $users)}}" method="POST">
                     @csrf
                     @method('PATCH')
+
+                    <div class="form-group row">
+                        <label for="name" class="col-md-6 col-form-label ">{{ __('Nom') }}</label>
+
+                        <div class="col-md-12">
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') ?? $users->name}}" required autocomplete="name" autofocus>
+
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="email" class="col-md-6 col-form-label ">{{ __('E-Mail Address') }}</label>
+
+                        <div class="col-md-12">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $users->email}}" required autocomplete="email" autofocus>
+
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    
                             @foreach ($roles as $role)
                             <div class="form-group form-check">
                                 <input type="checkbox" name="roles[]" id="{{ $role->id}}" class="form-check-input" value="{{ $role->id}}" 
@@ -21,7 +50,7 @@
                             </div>
                             
                             @endforeach
-                            <button type="submit" class="btn btn-primary">Modifier les roles</button>
+                            <button type="submit" class="btn btn-primary">Modifier les informations</button>
                   </form>
                    
                 </div>
