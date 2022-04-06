647cd8fbbf2321c666d42175f4b0622869bb9fa5
bechahedfiras <firas_bechahed@hotmail.com>
2022-04-06 16:19:17 +0000

    forgetpassword

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,10 +67,6 @@
                     </div>
                     <div class="form-group row">
                         <div class="col-md-12 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Login') }}
-                            </button>
-
                             @if (Route::has('password.request'))
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
