d6552facc902a48e9f28ab6a9941098a8957fd7a
bechahedfiras <firas_bechahed@hotmail.com>
2022-05-11 12:25:31 +0100

    sub

--- resources/views/users/subevents.blade.php
+++ resources/views/users/subevents.blade.php
@@ -5,9 +5,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-    <!-- MDB -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.css" rel="stylesheet" />
-
+    
     {{-- END CDN MDB --}}
     <div class="top-spacer">
 
@@ -42,87 +40,67 @@
                 </div>
 
             </div>
-            <div class="row">
+            
                 <div class="col">
                     <div class="section-title-header text-center mb-4">
                         <h1 class="section-title wow fadeInUp" data-wow-delay="0.2s">Sub Events</h1>
                     </div>
-                </div>
+                
 
 
                 <!-- Nested row for non-featured blog posts-->
-                <div class="row">
-                    @foreach ($subevents as $subevent)
-                        <div class="col-4">
-                            <!-- Blog post-->
-
-                            <div class="card mb-4">
-                                <a href="#!"><img class="card-img-top"
-                                        src="{{ asset('../storage/' . $subevent->photo) }}" alt="..." /></a>
-                                <div class="card-body">
-                                    <h2 class="card-title h4">{{ $subevent->label }}</h2>
-                                    <p class="card-text">{{ $subevent->description }}</p>
-                                    <form action="{{ url('/add-sub-event-to-cart') }}" method="POST">
-                                        @csrf
-                                        {{-- <input type="hidden" name="user_id" value="{{$event->id}}"> --}}
-                                        <div class="card-footer border-top-0 bg-transparent">
-                                            <input type="hidden" name="event_id" value="{{ $subevent->id }}">
-                                            <div class="text-center"><button type="submit"
-                                                    class="btn btn-primary mt-auto">ADD TO
-                                                    CART</button></div>
-                                        </div>
-                                    </form>
+                
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                  @foreach ($subevents as $subevent)
+                    <div class="col-4 mb-5   ">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <a href="#"><img class="card-img-top rounded img-fluid"  src="{{asset('../storage/'.$subevent->photo)}}" style=" height: 15rem;" alt="..." /></a>
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"> <a href="#">{{ $subevent->label }}</a></h5>
+                                    {{$subevent->price}} $
                                 </div>
                             </div>
+                            <!-- Product actions-->
+                            <form action="{{ url('/add-sub-event-to-cart') }}" method="POST">
+                              @csrf
+                              {{-- <input type="hidden" name="user_id" value="{{$event->id}}"> --}}
+                              <div class="card-footer border-top-0 bg-transparent">
+                                  <input type="hidden" name="event_id" value="{{ $subevent->id }}">
+                                  <div class="text-center"><button type="submit"
+                                          class="btn btn-primary mt-auto">ADD TO
+                                          CART</button></div>
+                              </div>
+                          </form>
                         </div>
-                    @endforeach
-
-                </div>
-
-            </div>
-            <div class="container">
-                <h5 class="section-title h1 text-center">TEAM</h5>
-                <div class="row">
-                    <!-- Team member -->
-                   
-
-      
-      
-
-         
-            <section>
-                <div class="row d-flex justify-content-center">
-                  <div class="col-md-10 col-xl-8 text-center">
-                    <h3 class="mb-4">TEAM</h3>
-                    <p class="mb-4 pb-2 mb-md-5 pb-md-0">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet
-                      numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum
-                      quisquam eum porro a pariatur veniam.
-                    </p>
-                  </div>
-                </div>
-              
-                <div class="row text-center">
-                    @foreach ($eventOgrs as $eventOgr)
-                  <div class="col-md-4 mb-5 mb-md-0 d-flex align-items-stretch">
-                    <div class="card testimonial-card ">
-                      <div class="card-up" style="background-color: #9d789b;  "></div>
-                      <div class="avatar mx-auto bg-white ">
-                        <img src="{{ asset('../storage/' . $eventOgr->user->profile_pic) }}"
-                          class="rounded img-fluid"  style="width:auto; height: 20rem;"/>
-                      </div>
-                      <div class="card-body">
-                        <h4 class="mb-4">{{ $eventOgr->user->name }}</h4>
-                        <hr />
-                        <p class="dark-grey-text mt-4">
-                          <i class="fas fa-quote-left pe-2"></i>{{ $eventOgr->user->email }}
-                        </p>
-                      </div>
+                        
                     </div>
-                  </div>
-                  @endforeach
+                    @endforeach 
                 </div>
-              </section>
+            
+            
+                <h5 class="section-title h1 text-center">TEAM</h5>
+                
+                    <!-- Team member -->
+            
+                <div class="row text-center">
+                  @foreach ($eventOgrs as $eventOgr)
+                  <div class="col-xl-3 col-sm-6 mb-5">
+                    <div class="bg-white rounded shadow-sm py-5 px-4"><img src="{{ asset('../storage/' . $eventOgr->user->profile_pic) }}" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                      <h5 class="mb-0">{{ $eventOgr->user->name }}</h5>
+                      <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                      </ul>
+                    </div>
+                    @endforeach
+                  </div>
+              
      
        
           </div>
@@ -162,8 +140,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-    <!-- MDB -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.css" rel="stylesheet" />
-
+    
     {{-- END CDN MDB --}}
             @endsection
